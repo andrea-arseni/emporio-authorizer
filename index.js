0663d2f449c6df6c445d684d05bb66b4051adecb
Andrea Arseni <andrea.arseni87@gmail.com>
2022-07-12 10:09:29 +0200

    Versione uno

--- index.js
+++ index.js
@@ -1,44 +1,73 @@
-// A simple token-based authorizer example to demonstrate how to use an authorization token
-// to allow or deny a request. In this example, the caller named 'user' is allowed to invoke
-// a request if the client-supplied token value is 'allow'. The caller is not allowed to invoke
-// the request if the token value is 'deny'. If the token value is 'unauthorized' or an empty
-// string, the authorizer function returns an HTTP 401 status code. For any other token value,
-// the authorizer returns an HTTP 500 status code.
-// Note that token values are case-sensitive.
+const jwt = require("jsonwebtoken");
+const mysql = require("mysql2/promise");
 
-exports.handler = function (event, context, callback) {
-    var token = event.authorizationToken;
-    switch (token) {
-        case "allow":
-            callback(null, generatePolicy("user", "Allow", event.methodArn));
-            break;
-        case "deny":
-            callback(null, generatePolicy("user", "Deny", event.methodArn));
-            break;
-        case "unauthorized":
-            callback("Unauthorized"); // Return a 401 Unauthorized response
-            break;
-        default:
-            callback("Error: Invalid token"); // Return a 500 Invalid token response
+let connection = null;
+
+mysql
+    .createConnection({
+        host: process.env.RDS_HOSTNAME,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+    })
+    .then((con) => (connection = con));
+
+exports.handler = async (event) => {
+    //se non siamo già connessi get connection
+    if (!connection) {
+        console.log("Nuova connessione");
+        connection = await mysql.createConnection({
+            host: process.env.RDS_HOSTNAME,
+            user: process.env.RDS_USERNAME,
+            password: process.env.RDS_PASSWORD,
+            database: process.env.RDS_DB_NAME,
+        });
+    }
+
+    // estrai il token dall'header
+    const token = extractTokenFromHeader(event.authorizationToken);
+    // se non lo trovi errore
+    if (!token) return "Unauthorized";
+    // verifica il token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded.exp || !decoded.jti) return "Unauthorized";
+        // se token scaduto dillo
+        const expTime = +decoded.exp * 1000;
+        const now = new Date().getTime();
+        if (expTime < now) return "Error: Invalid token";
+        // check a sistema se c'è quello user
+        const id = decoded.jti;
+        const queryExistingUser = `SELECT * FROM user WHERE id = '${id}'`;
+        const resQuery = await connection.execute(queryExistingUser);
+        const user = resQuery[0][0];
+        return user
+            ? generatePolicy(event.methodArn)
+            : {
+                  statusCode: 401,
+                  body: "User non trovato, accesso negato.",
+                  isBase64Encoded: false,
+              };
+    } catch (error) {
+        return "Unauthorized";
     }
 };
 
-// Help function to generate an IAM policy
-var generatePolicy = function (principalId, effect, resource) {
+// Generate IAM policy
+const generatePolicy = (resource) => {
     var authResponse = {};
-
-    authResponse.principalId = principalId;
-    if (effect && resource) {
-        var policyDocument = {};
-        policyDocument.Version = "2012-10-17";
-        policyDocument.Statement = [];
-        var statementOne = {};
-        statementOne.Action = "execute-api:Invoke";
-        statementOne.Effect = effect;
-        statementOne.Resource = resource;
-        policyDocument.Statement[0] = statementOne;
-        authResponse.policyDocument = policyDocument;
-    }
-
+    authResponse.principalId = "user";
+    var policyDocument = {};
+    policyDocument.Version = "2012-10-17";
+    policyDocument.Statement = [];
+    var statementOne = {};
+    statementOne.Action = "execute-api:Invoke";
+    statementOne.Effect = "Allow";
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
     return authResponse;
 };
+
+const extractTokenFromHeader = (authHeader) =>
+    authHeader.split(" ")[0] !== "Bearer" ? null : authHeader.split(" ")[1];
